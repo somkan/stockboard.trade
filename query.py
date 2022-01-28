20a1b07b3e3ecb596bb92c13672cf40264d25059
somkan <sosund@gmail.com>
2022-01-29 00:33:09 +0530

    Add files via upload

--- query.py
+++ query.py
@@ -15,6 +15,8 @@ import os
 
 myclient =os.environ.get("DBCONNECTION")
 myclient1=myclient
+
+print(myclient1)
 mydb = myclient1["StockboardDB"]
 #strategy1 = os.environ.get("STG1")
 strategy1 =mydb["strategy1"]
@@ -22,7 +24,5 @@ userid = os.environ.get("CLIENT1")
 print(mydb)
 print(strategy1)
 print(userid)
-key_signal = strategy1.find({}, {"stocks", "trigger_prices", "user", "Indicator"})
-#key_signal = strategy1.find({})
-for data in key_signal:
-    print(data["user"])
+#key_signal = strategy1.find({}, {"stocks", "trigger_prices", "user", "Indicator"})
+key_signal = strategy1.find({})
