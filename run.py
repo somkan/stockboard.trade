6b97a5c3e7ec1cf3074c6e82f6fa26eeaf485305
somkan <sosund@gmail.com>
2022-02-02 17:28:22 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import redis
 from urllib.parse import urlparse
 import json
 from flask import Flask, request, abort, Response, render_template
-from redis.commands.json.path import Path
+
 
 app = Flask(__name__)
 
@@ -11,15 +11,24 @@ url = urlparse(os.environ.get("REDISCLOUD_URL"))
 client = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True,
                      ssl_cert_reqs=None)
 
+@app.route('/strategy9', methods=['GET'])
+def get_respond():
+    data = request.args.get("stocks",None)
+    print("got stock {stocks}")
+    response = {}
+    response["MESSAGE"] = "Buy {stocks}"
+    return jsonify(response)
+
 
 @app.route('/strategy9', methods=['POST'])
 def get_webhook1():
     data = json.loads(request.data)
-    client.json().set('strategy:1',Path.rootPath(),data)
+
+    client.json().set('strategy:1',data)
     result = client.json().get('strategy:1')
     print(result)
 
 
 if __name__ == '__main__':
     #telegram("Stockboard Webhook ","Running")
-    app.run()
+    app.run(host="https://stockboard-app.herokuapp.com",debug=True)
